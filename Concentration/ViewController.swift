1dce37a406cef7a44632216fe7ee0dcbac68b220
zMy Number <stevenhuang11@gmail.com>
2018-09-01 22:18:02 -0700

    Basic layout + flip card function

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -19,7 +19,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1;
+        if let cardNumber = cardButtons.index(of: sender){
+            flipcard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }else{
+            print("Chosen card is not in cardButtons")
+        }
+    }
+    
+    func flipcard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji{
+            button.backgroundColor = UIColor.orange
+            button.setTitle("", for: UIControlState.normal)
+        }else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
 }
+
 
